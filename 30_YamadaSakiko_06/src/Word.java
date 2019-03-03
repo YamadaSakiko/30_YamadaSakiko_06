f58c250b5f950dfe90b50fac7dee2731fd7dfc89
山田早紀子 <s1105257@gmail.com>
2019-03-03 20:23:19 +0900

    ６回目課題の再提出

---
+++ 30_YamadaSakiko_06/src/Word.java
@@ -0,0 +1,9 @@
+public class Word {
+    public String word;
+    public String meaning;
+
+    public Word(String word,String meaning){
+        this.word=word;
+        this.meaning=meaning;
+    }
+}
